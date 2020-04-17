c0118baf73fed7294aaf18a31162180f6d4c637f
Anderson Rocha <andersonecomp091@gmail.com>
2020-04-16 22:06:12 -0300

    Finalizada navegação com uma navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import React from "react";
+
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
+import Header from "./common/Header";
 
 function App() {
-  const route = window.location.pathname;
-  if (route === "/about") {
-    return <AboutPage />;
+  function getPage(params) {
+    const route = window.location.pathname;
+    if (route === "/about") {
+      return <AboutPage />;
+    }
+    return <HomePage />;
   }
-  return <HomePage />;
+  return (
+    <div className="container-fluid">
+      <Header />
+      {getPage()}
+    </div>
+  );
 }
 
 export default App;
