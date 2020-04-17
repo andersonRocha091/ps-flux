97da367a8e57df463a54b5ae9ae7fc07289ed5bb
Anderson Rocha <andersonecomp091@gmail.com>
2020-04-16 22:41:59 -0300

    Finalizado componente da pagina de cursos

--- src/components/CoursesPage.jsx
+++ src/components/CoursesPage.jsx
@@ -19,7 +19,29 @@ class CoursesPage extends Component {
   }
 
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <>
+        <h2>Courses</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author ID</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.courses.map((course) => (
+              <tr key={course.id}>
+                <td>{course.title}</td>
+                <td>{course.authorId}</td>
+                <td>{course.category}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
   }
 }
 
