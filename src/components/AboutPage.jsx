9755942a9a64a01bbaf4e29a548ff71c84880a9c
Anderson Rocha <andersonecomp091@gmail.com>
2020-04-16 21:58:43 -0300

    Modularizando aplicação, criacao do component App navegação crua

---
+++ src/components/AboutPage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class AboutPage extends Component {
+  render() {
+    return (
+      <>
+        <h2>About</h2>
+        <p>This app uses react</p>
+      </>
+    );
+  }
+}
+
+export default AboutPage;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { render } from "react-dom";
-import HomePage from "./components/HomePage";
+import App from "./components/App";
 
-render(<HomePage />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
