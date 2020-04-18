15b0ad04566d2b9d126de57712471be3d69f772d
Anderson Rocha <andersonecomp091@gmail.com>
2020-04-18 18:59:58 -0300

    Adicionando um modelo super legal para validação de dados do formulário e exibição dos erros para o cliente

--- src/components/CourseForm.jsx
+++ src/components/CourseForm.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import TextInput from "./common/TextInput";
 import SelectInput from "./common/SelectInput";
 
-export default (props) => {
+function CourseForm(props) {
   const authorOptions = [
     { value: 1, label: "Cory House" },
     { value: 2, label: "Scott Allen" },
@@ -13,6 +15,7 @@ export default (props) => {
         id="title"
         name="title"
         label="Title"
+        error={props.errors.title}
         value={props.course.title}
         onChange={props.onChange}
       />
@@ -24,6 +27,7 @@ export default (props) => {
         value={props.course.authorId || ""}
         className="form-control"
         options={authorOptions}
+        error={props.errors.authorId}
         onChange={props.onChange}
       />
 
@@ -32,9 +36,19 @@ export default (props) => {
         name="category"
         label="Category"
         value={props.course.category}
+        error={props.errors.category}
         onChange={props.onChange}
       />
       <input type="submit" value="Save" className="btn btn-primary" />
     </form>
   );
+}
+
+CourseForm.propTypes = {
+  course: PropTypes.object.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired,
 };
+
+export default CourseForm;
--- src/components/ManageCoursePage.jsx
+++ src/components/ManageCoursePage.jsx
@@ -5,11 +5,12 @@ import CourseForm from "./CourseForm";
 import * as courseApi from "../api/courseApi";
 
 export default (props) => {
+  const [errors, setErrors] = useState({});
   const [course, setCourse] = useState({
     id: null,
     slug: "",
     title: "",
-    authorId: "null",
+    authorId: null,
     category: "",
   });
 
@@ -23,16 +24,30 @@ export default (props) => {
 
   function handleSubmit(event) {
     event.preventDefault();
+    if (!formIsValid()) return;
     courseApi.saveCourse(course).then(() => {
       props.history.push("/courses");
       toast.success("Course successfully saved.");
     });
   }
 
+  function formIsValid() {
+    const _errors = {};
+    debugger;
+    if (!course.title) _errors.title = "Title is required";
+    if (!course.authorId) _errors.authorId = "Author ID is required";
+    if (!course.category) _errors.category = "Category is required";
+
+    setErrors(_errors);
+
+    return Object.keys(_errors).length === 0;
+  }
+
   return (
     <>
       <h2>Manage Course</h2>
       <CourseForm
+        errors={errors}
         course={course}
         onChange={handleChange}
         onSubmit={handleSubmit}
