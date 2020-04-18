3e4e7c9d23e59716c3addfb8787d9e903470e3e6
Anderson Rocha <andersonecomp091@gmail.com>
2020-04-17 22:01:53 -0300

    Convertendo course page de classe para um componente funcional usando hooks

--- src/components/CoursesPage.jsx
+++ src/components/CoursesPage.jsx
@@ -1,48 +1,38 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
 
-class CoursesPage extends Component {
-  //   constructor(props) {
-  //     super(props);
-  //     this.state = {
-  //       courses: [],
-  //     };
-  //   }
-  state = {
-    courses: [],
-  };
+function CoursesPage() {
+  const [courses, setCourses] = useState([]);
 
-  componentDidMount() {
-    getCourses().then((courses) => {
-      this.setState({ courses: courses });
+  useEffect(() => {
+    getCourses().then((_courses) => {
+      setCourses(_courses);
     });
-  }
+  }, []);
 
-  render() {
-    return (
-      <>
-        <h2>Courses</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author ID</th>
-              <th>Category</th>
+  return (
+    <>
+      <h2>Courses</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author ID</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map((course) => (
+            <tr key={course.id}>
+              <td>{course.title}</td>
+              <td>{course.authorId}</td>
+              <td>{course.category}</td>
             </tr>
-          </thead>
-          <tbody>
-            {this.state.courses.map((course) => (
-              <tr key={course.id}>
-                <td>{course.title}</td>
-                <td>{course.authorId}</td>
-                <td>{course.category}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </>
-    );
-  }
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default CoursesPage;
