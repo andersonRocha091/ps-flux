cbe01ae3e1582dbaf1d5ce623c7fae36360f3dde
Anderson Rocha <andersonecomp091@gmail.com>
2020-04-19 22:39:04 -0300

    Criando a classe store para registro , remoção, e emissão, de eventos

---
+++ src/stores/CourseStore.js
@@ -0,0 +1,21 @@
+import { EventEmitter } from "events";
+
+const CHANGE_EVENT = "change";
+
+class CourseStore extends EventEmitter {
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
+  }
+
+  removeChangeListener(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+
+  emitChange() {
+    this.emit(CHANGE_EVENT);
+  }
+}
+
+const store = new CourseStore();
+
+export default store;
